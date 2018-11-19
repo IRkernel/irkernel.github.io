dde2bc4180a153147f2d43e26ea13bcaf7f4d33e
Philipp A <flying-sheep@web.de>
2018-11-19 15:08:26 +0100

    We’re on CRAN!

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
-'use strict';
+'use strict'
 
 document.addEventListener('DOMContentLoaded', function() {
 	var blocks = document.querySelectorAll('pre code, code[class]')
-	Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+	for (const block of blocks) {
+		hljs.highlightBlock(block)
+	}
 })
 
 function childrenWithClass(e, clazz) {
-	return Array.prototype.filter.call(e.children, function(c) {
-		return c.classList.contains(clazz)
-	})
-}
-
-function MaterialTabsFixed(element) {
-	MaterialTabs.call(this, element)
+	return Array.from(e.children)
+		.filter(c => c.classList.contains(clazz))
 }
 
 function setTabActive(tab, ctx) {
 	var frag = tab.href.split('#')[1]
-	var panel = ctx.element_.querySelector('#' + frag)
+	var panel = ctx.element_.querySelector(`#${frag}`)
 	ctx.resetTabState_()
 	ctx.resetPanelState_()
 	tab.classList.add(ctx.CssClasses_.ACTIVE_CLASS)
@@ -25,72 +22,84 @@ function setTabActive(tab, ctx) {
 	return frag
 }
 
-var tabCtxs = new WeakMap
+const tabCtxs = new WeakMap
 
 function loadTab() {
-	var activePanel = document.querySelector(window.location.hash)
+	const activePanel = document.querySelector(window.location.hash)
 	if (!window.location.hash || !activePanel) return
 	
-	var tabSpine = []
-	for (var elem = activePanel; elem !== document.documentElement; elem = elem.parentElement) {
+	const tabSpine = []
+	for (let elem = activePanel; elem !== document.documentElement; elem = elem.parentElement) {
 		if (elem.classList.contains('mdl-tabs__panel')) {
-			var tab = document.querySelector('a.mdl-tabs__tab[href="#' + elem.getAttribute('id') +'"]')
-			if (tab) {
-				tabSpine.push(tab)
-			}
+			const tab = document.querySelector(`a.mdl-tabs__tab[href="#${elem.getAttribute('id')}"]`)
+			if (tab) tabSpine.push(tab)
 		}
 	}
 	
 	console.log(tabSpine)
-	if (!tabSpine) return
 	
-	tabSpine.forEach(function(tab) {
+	for (const tab of tabSpine) {
 		setTabActive(tab, tabCtxs.get(tab))
-	})
+	}
 }
 
-MaterialTabsFixed.prototype = Object.create(MaterialTabs.prototype)
-MaterialTabsFixed.prototype.constructor = MaterialTabsFixed
-MaterialTabsFixed.prototype.initTabs_ = function() {
-	if (this.element_.classList.contains(this.CssClasses_.MDL_JS_RIPPLE_EFFECT)) {
-		this.element_.classList.add(
-			this.CssClasses_.MDL_JS_RIPPLE_EFFECT_IGNORE_EVENTS);
+function updateLinks() {
+	for (const tab of document.querySelectorAll('.mdl-tabs__tab')) {
+		const frag = tab.href.split('#')[1]
+		const links = document.querySelectorAll(`a:not(.mdl-tabs__tab)[href="#${frag}"]`)
+		for (const link of links) {
+			link.addEventListener('click', (e) => {
+				e.preventDefault()
+				const frag = setTabActive(tab, tabCtxs.get(tab))
+				window.history.pushState(frag, '', `#${frag}`)
+			})
+		}
 	}
-	
-	var tabBar = childrenWithClass(this.element_, 'mdl-tabs__tab-bar')[0]
-	
-	this.tabs_ = childrenWithClass(tabBar, this.CssClasses_.TAB_CLASS)
-	this.panels_ = childrenWithClass(this.element_, this.CssClasses_.PANEL_CLASS)
-	
-	for (var i = 0; i < this.tabs_.length; i++) {
-		new MaterialTab(this.tabs_[i], this)
-	}
-	
-	this.element_.classList.add(this.CssClasses_.UPGRADED_CLASS)
 }
 
-function MaterialTab(tab, ctx) {
-	if (tab) {
+class MaterialTabsFixed extends MaterialTabs {
+	initTabs_() {
+		if (this.element_.classList.contains(this.CssClasses_.MDL_JS_RIPPLE_EFFECT)) {
+			this.element_.classList.add(
+				this.CssClasses_.MDL_JS_RIPPLE_EFFECT_IGNORE_EVENTS)
+		}
+		
+		const tabBar = childrenWithClass(this.element_, 'mdl-tabs__tab-bar')[0]
+		
+		this.tabs_ = childrenWithClass(tabBar, this.CssClasses_.TAB_CLASS)
+		this.panels_ = childrenWithClass(this.element_, this.CssClasses_.PANEL_CLASS)
+		
+		for (const tab of this.tabs_) {
+			new MaterialTab(tab, this)
+		}
+		
+		this.element_.classList.add(this.CssClasses_.UPGRADED_CLASS)
+	}
+}
+
+class MaterialTab {
+	constructor(tab, ctx) {
+		if (!tab) return
 		if (ctx.element_.classList.contains(ctx.CssClasses_.MDL_JS_RIPPLE_EFFECT)) {
-			var rippleContainer = document.createElement('span')
+			const rippleContainer = document.createElement('span')
 			rippleContainer.classList.add(ctx.CssClasses_.MDL_RIPPLE_CONTAINER)
 			rippleContainer.classList.add(ctx.CssClasses_.MDL_JS_RIPPLE_EFFECT)
-			var ripple = document.createElement('span')
+			const ripple = document.createElement('span')
 			ripple.classList.add(ctx.CssClasses_.MDL_RIPPLE)
 			rippleContainer.appendChild(ripple)
 			tab.appendChild(rippleContainer)
 		}
 		
-		tab.addEventListener('click', function(e) {
+		tab.addEventListener('click', (e) => {
 			e.preventDefault()
-			var frag = setTabActive(tab, ctx)
-			window.history.pushState(frag, '', '#' + frag)
+			const frag = setTabActive(tab, ctx)
+			window.history.pushState(frag, '', `#${frag}`)
 		})
 		
 		tabCtxs.set(tab, ctx)
 	}
 }
-
+		
 // The component registers itself. It can assume componentHandler is available
 // in the global scope.
 componentHandler.register({
@@ -99,9 +108,10 @@ componentHandler.register({
 	cssClass: 'mdl-js-tabs-fixed',
 })
 
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
 	componentHandler.downgradeElements(document.querySelectorAll('.mdl-js-ripple-effect'))
 	componentHandler.upgradeElements(document.querySelectorAll('.mdl-js-ripple-effect'))
-	
+
 	loadTab()
+	updateLinks()
 })
